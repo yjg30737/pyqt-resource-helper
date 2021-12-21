6b373760846dba057b941e6d723e4f54fbdf4a5e
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-21 13:29:49 +0900

    Fix problem related to index and argument

--- pyqt_resource_helper/pyqtResourceHelper.py
+++ pyqt_resource_helper/pyqtResourceHelper.py
@@ -15,19 +15,22 @@ class PyQtResourceHelper:
     def setStyleSheet(widgets: list, style_sheets: list):
         caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(1)).filename)
 
-        def setStyleSheetOf(i):
+        def getStyleSheetOf(i):
             css_file_path = os.path.join(caller_path, style_sheets[i])
             css_file = open(css_file_path)
             css_code = css_file.read()
             css_file.close()
-            widgets[i].setStyleSheet(css_code)
+            return css_code
 
         if len(style_sheets) == 1:
             for i in range(len(widgets)):
-                setStyleSheetOf(0)
+                css_code = getStyleSheetOf(0)
+                widgets[i].setStyleSheet(css_code)
         elif len(style_sheets) == 2:
             for i in range(len(widgets)):
-                setStyleSheetOf(i % 2)
+                css_code = getStyleSheetOf(i % 2)
+                widgets[i % 2].setStyleSheet(css_code)
         else:
             for i in range(len(widgets)):
-                setStyleSheetOf(i)
+                css_code = getStyleSheetOf(i)
+                widgets[i].setStyleSheet(css_code)
