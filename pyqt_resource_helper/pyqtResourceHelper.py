21bf33d8ff130b57e2b58e5fb9f33d7c652ebfb6
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-21 13:04:09 +0900

    Support various amount of arguments

--- pyqt_resource_helper/pyqtResourceHelper.py
+++ pyqt_resource_helper/pyqtResourceHelper.py
@@ -14,9 +14,26 @@ class PyQtResourceHelper:
     @staticmethod
     def setStyleSheet(widgets: list, style_sheets: list):
         caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(1)).filename)
-        for i in range(len(widgets)):
-            css_file_path = os.path.join(caller_path, style_sheets[i])
-            css_file = open(css_file_path)
-            css_code = css_file.read()
-            css_file.close()
-            widgets[i].setStyleSheet(css_code)
+        if len(style_sheets) == 1:
+            for i in range(len(widgets)):
+                css_file_path = os.path.join(caller_path, style_sheets[0])
+                css_file = open(css_file_path)
+                css_code = css_file.read()
+                css_file.close()
+                widgets[i].setStyleSheet(css_code)
+        elif len(style_sheets) == 2:
+            caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(1)).filename)
+            for i in range(len(widgets)):
+                css_file_path = os.path.join(caller_path, style_sheets[i % 2])
+                css_file = open(css_file_path)
+                css_code = css_file.read()
+                css_file.close()
+                widgets[i].setStyleSheet(css_code)
+        else:
+            caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(1)).filename)
+            for i in range(len(widgets)):
+                css_file_path = os.path.join(caller_path, style_sheets[i])
+                css_file = open(css_file_path)
+                css_code = css_file.read()
+                css_file.close()
+                widgets[i].setStyleSheet(css_code)
